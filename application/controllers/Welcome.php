3cc2a7876e72aaa47ac1271566bd093f2c475e39
agungdh <agungdh@live.com>
2018-02-08 19:17:21 +0700

    update
    fix ke v1.1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,51 +9,26 @@ class Welcome extends CI_Controller {
         parent::__construct($config);
         $this->load->library('curl');
 		$this->API=base_url('api');
+        $this->load->library('user_agent');
 	}
 
-	function index(){
-        $this->output->set_content_type('text/plain');
-
-        $data['ip'] = $this->input->ip_address();
-
-        $json = json_decode($this->curl->simple_get($this->API, $data, array(CURLOPT_BUFFERSIZE => 10)));
-
-        foreach ($json as $key => $value) {
-            echo $key . " = " . $value . "\n";
-        }
-    }
-
-    function browser(){
-        $this->load->view('browser');
-    }
-
-    function check() {
-        // foreach (getallheaders() as $name => $value) {
-        //     echo "$name: $value\n";
-        // }
-        // echo $_SERVER['HTTP_USER_AGENT'];
-        $this->load->library('user_agent');
-
-        if ($this->agent->is_browser())
+    function index() {
+        if ($this->agent->is_browser() || $this->agent->is_robot() || $this->agent->is_mobile())
         {
-                $agent = $this->agent->browser().' '.$this->agent->version();
-        }
-        elseif ($this->agent->is_robot())
-        {
-                $agent = $this->agent->robot();
-        }
-        elseif ($this->agent->is_mobile())
-        {
-                $agent = $this->agent->mobile();
+                $this->load->view('browser');
         }
         else
         {
-                $agent = 'Unidentified User Agent';
+                $this->output->set_content_type('text/plain');
+
+                $data['ip'] = $this->input->ip_address();
+
+                $json = json_decode($this->curl->simple_get($this->API, $data, array(CURLOPT_BUFFERSIZE => 10)));
+
+                foreach ($json as $key => $value) {
+                    echo $key . " = " . $value . "\n";
+                }
+
         }
-
-        echo $agent;
-
-        echo $this->agent->platform(); // Platform info (Windows, Linux, Mac, etc.)
-
     }
 }
