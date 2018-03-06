a37ed9e483cb54ea693ca760b5cbbac3c319e2a4
agungdh <agungdh@live.com>
2018-03-06 18:57:23 +0700

    update

--- application/views/browser.php
+++ application/views/browser.php
@@ -3,19 +3,43 @@
 <head>
 	<meta charset="utf-8"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+		.modal {
+		    display:    none;
+		    position:   fixed;
+		    z-index:    1000;
+		    top:        0;
+		    left:       0;
+		    height:     100%;
+		    width:      100%;
+		    background: rgba( 255, 255, 255, .8 ) 
+		                url('<?php echo base_url("assets/loading.gif"); ?>') 
+		                50% 50% 
+		                no-repeat;
+		}
+
+		body.loading {
+		    overflow: hidden;   
+		}
+
+		body.loading .modal {
+		    display: block;
+		}
+	</style>
 	<script type="text/javascript" src="<?php echo base_url('assets/jquery.js'); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
 	<link href="<?php echo base_url('assets/flag-icon-css-master/css/flag-icon.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
 	<title>AgungDH</title>
 </head>
-<body>
+<body class="loading">
 	<div class="container">
 		<center>
 			<h1>IP Checker</h1>
 			<div id="data"></div>	
 		</center>
 	</div>
+	<div class="modal"></div>
 	<script type="text/javascript">
 	var peta;
 	var ip;
@@ -36,6 +60,7 @@
 				$("#data").append(
 		 			ip + operator + peta + lokasi1 + ', ' + data.name
 		 		);
+		 		$("body").attr("class", "");
 			});
  		});
 		
