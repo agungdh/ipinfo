de22759385d71777df87bfe2406b71532ea4e587
agungdh <agungdh@live.com>
2018-03-06 16:41:51 +0700

    update

--- application/views/browser.php
+++ application/views/browser.php
@@ -8,18 +8,29 @@
 <body>
 	<div id="data"></div>
 	<script type="text/javascript">
-	$(document).ready(function(){
-		$.getJSON('https://ipinfo.io', function(data){
-			var peta = '<iframe src = "https://maps.google.com/maps?q='+data.loc+'&hl=es;z=14&amp;output=embed"></iframe>';
-			var ip = '<p>IP: '+data.ip+'</p>';
-			var operator = '<p>Operator: '+data.org+'</p>';
-			var lokasi = '<p>Lokasi: <span class="flag-icon flag-icon-'+data.country.toLowerCase()+'"></span> '+data.city+', '+data.region+'</p>';
+	var peta;
+	var ip;
+	var operator;
+	var negara;
+	var lokasi1;
+	var lokasi2;
 
-	 		$("#data").append(
-	 			peta + ip + operator + lokasi
-	 		);
-			
-		});
+	$.getJSON('https://ipinfo.io', function(data){
+		peta = '<iframe src = "https://maps.google.com/maps?q='+data.loc+'&hl=es;z=14&amp;output=embed"></iframe>';
+		ip = '<p>IP: '+data.ip+'</p>';
+		operator = '<p>Operator: '+data.org+'</p>';
+		negara = data.country.toLowerCase();
+		lokasi1 = '<p>Lokasi: <span class="flag-icon flag-icon-'+negara+'"></span> '+data.city+', '+data.region;
+		lokasi2 = '</p>';
+		$(document).ready(function(){
+			$.getJSON('https://restcountries.eu/rest/v2/alpha/'+negara, function(data){
+				var fullnegara = data.name;
+				$("#data").append(
+		 			peta + ip + operator + lokasi1 + ', ' + data.name + lokasi2
+		 		);
+			});
+ 		});
+		
 	});
 	</script>
 </body>
