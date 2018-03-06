503b5e0e8cdd107af98f5940d61183fceb3e94a7
agungdh <agungdh@live.com>
2018-03-06 16:27:34 +0700

    update

--- application/views/browser.php
+++ application/views/browser.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<script type="text/javascript" src="<?php echo base_url('assets/jquery.js'); ?>"></script>
+	<link href="<?php echo base_url('assets/flag-icon-css-master/css/flag-icon.css'); ?>" rel="stylesheet">
 	<title>AgungDH</title>
 </head>
 <body>
@@ -9,9 +10,15 @@
 	<script type="text/javascript">
 	$(document).ready(function(){
 		$.getJSON('https://ipinfo.io', function(data){
-	 		$.each(data, function(index, value) {
-	 			$("#data").append(index + ' = ' + value + '<br>');
-			}); 
+			var peta = '<iframe src = "https://maps.google.com/maps?q='+data.loc+'&hl=es;z=14&amp;output=embed"></iframe>';
+			var ip = '<p>IP: '+data.ip+'</p>';
+			var operator = '<p>Operator: '+data.org+'</p>';
+			var lokasi = '<p>Lokasi: <span class="flag-icon flag-icon-'+data.country.toLowerCase()+'"></span> '+data.city+', '+data.region+'</p>';
+
+	 		$("#data").append(
+	 			peta + ip + operator + lokasi
+	 		);
+			
 		});
 	});
 	</script>
